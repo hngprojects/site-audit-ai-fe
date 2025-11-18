6646fe2ddcb26dc98ac6aceb5218c4d3acabddf7
Toluwalashe <asheogunleye02@gmail.com>
2025-11-18 03:53:28 +0100

    refactor: revamp header component for flexibility and landing page use

---
+++ lib/nav-link-data.tsx
@@ -0,0 +1,9 @@
+export const navLinksWaitlist = [
+  { href: "/", label: "Home" },
+  { href: "/waitlist#benefits", label: "Benefits" },
+];
+
+export const navLinksLanding = [
+  { href: "/", label: "Home" },
+  { href: "/how-it-works", label: "How It Works" },
+];
