a41d471e5d8e96600519552bc993c89722c3eb6d
AgboolaAgbeniga <agboolaagbeniga@gmail.com>
2025-11-19 07:24:49 +0100

    feat: add footer links data

--- components/waitlist/landing-footer.tsx
+++ components/waitlist/landing-footer.tsx
@@ -13,59 +13,79 @@ const LandingFooter = () => {
 
   return (
     <footer>
-      <div
-        id="footer-app"
-        className="max-w-6xl h-[410px] bg-[#E85238] rounded-4xl mx-auto relative top-24 font-sans"
-      >
-        {/* left column */}
-        <div className="p-8 md:p-12 flex flex-col items-cente justify-between h-full">
-          <div className="flex-1">
-            <h2 className="text-[48px] font-sans font-semibold text-white">
-              Audit Your Website Instantly
-            </h2>
-            <p className="text-[18px] text-white mt-4">
-              Download the app and let AI analyze your site, uncover
-              opportunities, and boost performance, all in seconds.
-            </p>
+      <div>
+        <div
+          id="footer-app"
+          className=" hidden max-w-6xl max-h-[410px] bg-[#E85238] rounded-4xl md:mx-20 lg:mx-40 relative top-24 font-sans  flex-col md:flex-row items-center justify-between"
+        >
+          {/* left column */}
+          <div className="p-8 md:p-12 flex flex-col  h-full gap-6">
+            <div className="">
+              <h2 className="text-[clamp(24px,3.35vw+0.2px,48px)] max-w-[500px] font-sans font-semibold text-white leading-12">
+                Audit Your Website Instantly
+              </h2>
+              <p className="text-[18px] max-w-[497px] text-white mt-4">
+                Download the app and let AI analyze your site, uncover
+                opportunities, and boost performance, all in seconds.
+              </p>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-4 mt-6 md:mt-0">
+              <Link
+                href="#"
+                className="bg-black text-white px-6 py-3 rounded-lg flex items-center gap-2 text-[11px]"
+              >
+                <Image
+                  src="/assets/images/google-play.svg"
+                  alt="Google Play"
+                  width={24}
+                  height={24}
+                />
+                <div>
+                  Get it on{" "}
+                  <p className="text-base font-semibold">Google Play</p>
+                </div>
+              </Link>
+              <Link
+                href="#"
+                className="bg-black  text-white px-6 py-3 rounded-lg flex items-center gap-2 text-[11px]"
+              >
+                <Image
+                  src="/assets/images/apple.svg"
+                  alt="App Store"
+                  width={24}
+                  height={24}
+                />
+                <div>
+                  Download on the{" "}
+                  <p className="text-base font-semibold">App Store</p>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className="flex flex-col sm:flex-row gap-4 mt-6 md:mt-0">
-            <Link
-              href="#"
-              className="bg-black text-white px-6 py-3 rounded-lg flex items-center gap-2 text-[11px]"
-            >
-              <Image
-                src="/assets/images/google-play.svg"
-                alt="Google Play"
-                width={24}
-                height={24}
-              />
-              <div>
-                Get it on <p className="text-base font-semibold">Google Play</p>
-              </div>
-            </Link>
-            <Link
-              href="#"
-              className="bg-black  text-white px-6 py-3 rounded-lg flex items-center gap-2 text-[11px]"
-            >
-              <Image
-                src="/assets/images/apple.svg"
-                alt="App Store"
-                width={24}
-                height={24}
-              />
-              <div>
-                Download on the{" "}
-                <p className="text-base font-semibold">App Store</p>
-              </div>
-            </Link>
+
+          {/* right coluumn */}
+
+          <div>
+            <Image
+              src="/assets/images/wavey.svg"
+              alt="Footer"
+              width={500}
+              height={500}
+              className="absolu h-[450px] w-lg overflow-x-visible trans"
+            />
+            <Image
+              src="/assets/images/group_phone.svg"
+              alt="Footer"
+              width={500}
+              height={500}
+              className="absolute bottom-0 right-0 "
+            />
           </div>
         </div>
-
-        {/* right coluumn */}
-        <div></div>
       </div>
+
       <section className="bg-[#0A0A0B] pt-40">
-        <div className="flex items-start justify-between max-w-[1440px] mx-auto font-sans py-8 flex-col md:flex-row md:justify-between md:px-12 md:mb-20">
+        <div className="flex items-start justify-between max-w-[1440px] mx-auto font-sans py-8 px-11 md:px-12  flex-col sm:flex-row md:justify-between md:mb-20 gap-8">
           <div>
             <Link
               href="/"
@@ -73,21 +93,18 @@ const LandingFooter = () => {
               className="flex items-center gap-2"
             >
               <Image
-                src="/assets/images/Logo_single.svg"
+                src="/assets/images/landing/new_logo.svg"
                 alt="Site Audit AI Logo"
                 width={140}
                 height={140}
-                className="w-[63.5]"
+                className="w-[77.69]"
               />
-              <p className="font-sans text-[#ECE9EC] text-[40px] font-bold">
-                Sitelytics
-              </p>
             </Link>
           </div>
-          <div className="flex flex-col md:flex-row gap-20">
+          <div className="flex flex-col sm:flex-row  gap-8 sm:gap-20">
             {footerLinks.map((column) => (
               <div key={column.title}>
-                <h3 className="text-[32px] text-[#E5E9EC] font-bold mb-[16.45px]">
+                <h3 className="text-lg sm:text-[clamp(16px,2.299vw-1.11px,32px)] text-[#E5E9EC] font-bold sm:mb-[16.45px]">
                   {column.title}
                 </h3>
                 <ul className="space-y-2">
@@ -95,7 +112,7 @@ const LandingFooter = () => {
                     <li key={link.name}>
                       <Link
                         href={link.href}
-                        className="text-[21.9px] text-[#9FA1A2] font-medium"
+                        className="text-xs sm:text-[clamp(14px,1.149vw+5.45px,22px)] text-[#9FA1A2] font-medium"
                       >
                         {link.name}
                       </Link>
@@ -107,12 +124,12 @@ const LandingFooter = () => {
           </div>
         </div>
 
-        <div className=" max-w-[1440px] mx-auto md:px-12 ">
+        <div className=" max-w-[1440px] mx-auto px-11 md:px-12 ">
           <div
             id="footer-nav"
             className=" font-sans  flex flex-col items-center justify-center gap-6 min:h-32 md:flex-row md:justify-between border-b border-b-[#f5e9e9]/45 md:my-6 py-8 "
           >
-            <p className="text-[#f5e9e9] text-sm md:text-xl">
+            <p className="text-[#f5e9e9] text-xs md:text-xl">
               &copy;{new Date().getFullYear()} HNG Tech Limited. All rights
               reserved.
             </p>
@@ -140,7 +157,7 @@ const LandingFooter = () => {
           </div>
         </div>
 
-        <div className="h-1"></div>
+        <div className="h-4 "></div>
       </section>
     </footer>
   );
