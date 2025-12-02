eb99abd2f2f3aef6851f4f612b5bc87f1c62ad34
Orion <holupeter2@gmail.com>
2025-12-02 21:44:33 +0100

    feat: Improve testimonial section alignment, navigation and data update

--- lib/testimonials-data.ts
+++ lib/testimonials-data.ts
@@ -22,23 +22,44 @@ export const testimonialsData = [
   },
   {
     quote:
-      "Sitelytics helped me understand what was wrong with my website and how to fix it. The clarity it gave transformed my online presence.",
-    name: "Okoye Amina",
+      "I didn’t even realize half the issues hurting my sales until Sitelytics pointed them out. The report was so easy to understand, and I made updates the same day.",
+    name: "Michelle O’ Brien",
     title: "Business Owner",
-    avatar: "/assets/images/landing/avatar-3.jpg",
+    avatar: "/assets/images/landing/avatar-4.png",
   },
   {
     quote:
-      "Sitelytics showed me the issues slowing down my website. The recommendations were clear, practical, and easy to Hire a pro.",
-    name: "Carter Emma",
+      "The audit reports make it easy to show clients exactly what’s wrong and what we need to fix. It has streamlined our workflow dramatically.",
+    name: "Ben Driver",
     title: "Business Consultant",
-    avatar: "/assets/images/landing/avatar-2.jpg",
+    avatar: "/assets/images/landing/avatar-5.png",
   },
   {
     quote:
-      "Using Sitelytics feels like having a personal web expert. It made updating my website smooth and stress-free.",
-    name: "Brooks Liam",
-    title: "Fashion Content Creator",
-    avatar: "/assets/images/landing/avatar-1.jpg",
+      "Brands kept asking for my media kit page and sometimes it wouldn’t load properly. This audit helped me fix everything in under an hour. Super easy to use!",
+    name: "Tara Carison",
+    title: "Social Media Influencer",
+    avatar: "/assets/images/landing/avatar-6.png",
+  },
+  {
+    quote:
+      "Running a Shopify store means speed equals money. Sitelytics showed me every bottleneck hurting my conversions. My site loads twice as fast now",
+    name: "Daniel Reddington",
+    title: "E-Commerce Entrepreneur",
+    avatar: "/assets/images/landing/avatar-7.png",
+  },
+  {
+    quote:
+      "My therapy website had broken links and slow pages I didn’t know about. Sitelytics helped me fix everything, and now new clients find me easier on Google.",
+    name: "Dr Joseph Fields",
+    title: "Business Owner",
+    avatar: "/assets/images/landing/avatar-8.png",
+  },
+  {
+    quote:
+      "My landing pages weren’t converting until Sitelytics pointed out key issues. Fixing them increased my email opt-ins by 40%. This app pays for itself every single day.",
+    name: "Kara Fields",
+    title: "Online Course Creator",
+    avatar: "/assets/images/landing/avatar-9.png",
   },
 ];
